73e2aeff2e922957d72982772015c5109eded03c
SantiagoPozo <santiago.p.t@gmail.com>
2025-09-05 13:43:01 +0200

    Add GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://santiagopozo.github.io",
+  base: "/maria-teatro-educacion",
+  build: {
+    assets: "assets",
+  },
+});

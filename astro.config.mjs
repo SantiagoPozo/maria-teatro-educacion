49387e0b82e412c7de400265896a8b1d3cc9f431
SantiagoPozo <santiago.p.t@gmail.com>
2025-09-05 20:43:39 +0200

    Fix image paths and baseUrl usage, update about.md title, and clean up collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,14 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://santiagopozo.github.io",
-  base: process.env.CI ? "/maria-teatro-educacion" : "/",
+  base: process.env.CI ? "/maria-teatro-educacion/" : "/",
   build: {
     assets: "_astro",
   },
   vite: {
     css: {
       preprocessorOptions: {
-        sass: {
-          // Asegurar que Sass se compile correctamente
-        },
+        sass: {},
       },
     },
   },

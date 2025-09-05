2178cda9e99a7f6cb5df970be9ed98d006045ae3
SantiagoPozo <santiago.p.t@gmail.com>
2025-09-05 18:40:10 +0200

    Fix NODE_ENV configuration in GitHub Actions workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://santiagopozo.github.io",
-  base: process.env.NODE_ENV === "production" ? "/maria-teatro-educacion" : "/",
+  base: process.env.CI ? "/maria-teatro-educacion" : "/",
   build: {
     assets: "_astro",
   },

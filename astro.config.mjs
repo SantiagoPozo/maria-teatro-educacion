f86e4f680bcfcf2470d31eed1c0ee6c9ede516cf
SantiagoPozo <santiago.p.t@gmail.com>
2025-09-09 20:01:48 +0200

    Fix baseurl and config for mariapozotavora.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://santiagopozo.github.io",
-  base: process.env.CI ? "/maria-teatro-educacion/" : "/",
+  site: "https://mariapozotavora.com",
+  base: "/",
   build: {
     assets: "_astro",
   },
